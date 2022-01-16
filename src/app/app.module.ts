8548ccc37b548107a2e876e6b9f0643801c7e7ce
WayneJr <blairu92@gmail.com>
2022-01-16 09:22:32 +0100

    kinda initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteSquareComponent } from './quote-square/quote-square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteSquareComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/quote-square/quote-square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-square',
+  templateUrl: './quote-square.component.html',
+  styleUrls: ['./quote-square.component.css']
+})
+export class QuoteSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
